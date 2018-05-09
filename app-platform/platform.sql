df37b839d7cafc3a036e975ef323b71fe1d49e76
yangyijun <1012293987@qq.com>
2018-05-09 09:51:50 +0800

    删除多余的文件

--- app-platform/platform.sql
+++ app-platform/platform.sql
@@ -81,6 +81,19 @@ status int(4) NOT NULL COMMENT '状态',
 PRIMARY KEY (id)
 ) COMMENT '商品品牌表' ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+#图片表
+drop table if exists tb_picture;
+CREATE TABLE tb_picture (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+url varchar(255) NOT NULL COMMENT '图片URL',
+name varchar(50) NOT NULL COMMENT '图片名称',
+description varchar(255) DEFAULT NULL COMMENT '图片描述',
+good_id bigint(20) NOT NULL COMMENT '商品ID',
+ctime bigint(20) NOT NULL COMMENT '创建时间',
+creator bigint(20) NOT NULL COMMENT '创建人',
+PRIMARY KEY (id)
+) COMMENT '图片表' ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 #商品表
 drop table if exists tb_good;
 CREATE TABLE tb_good (
