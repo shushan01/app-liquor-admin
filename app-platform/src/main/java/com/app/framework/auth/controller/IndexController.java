53e143c586fef1318245167293629a47abcbb858
yangyijun <1012293987@qq.com>
2018-05-09 16:09:09 +0800

    添加前端测试页面

--- app-platform/src/main/java/com/app/framework/auth/controller/IndexController.java
+++ app-platform/src/main/java/com/app/framework/auth/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.app.framework.auth.controller;
 
 import com.app.framework.base.BaseController;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
+@RequiresAuthentication
 public class IndexController extends BaseController {
 
 
