aa785fe35358854a50185dea98bc75ed0381bc6e
yangyijun <1012293987@qq.com>
2018-05-13 14:57:24 +0800

    添加商品类别管理页面

--- app-view/src/components/common/Sidebar.vue
+++ app-view/src/components/common/Sidebar.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="sidebar">
         <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#324157"
-            text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
+                 text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
             <template v-for="item in items">
                 <template v-if="item.subs">
                     <el-submenu :index="item.index" :key="item.index">
                         <template slot="title">
-                            <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
+                            <i :class="item.icon"></i>{{ item.title }}
                         </template>
-                        <el-menu-item v-for="(subItem,i) in item.subs" :key="i" :index="subItem.index">
-                            {{ subItem.title }}
+                        <el-menu-item v-for="subItem in item.subs" :index="subItem.index">
+                            <i :class="subItem.icon"></i>{{ subItem.title }}
                         </el-menu-item>
                     </el-submenu>
                 </template>
                 <template v-else>
                     <el-menu-item :index="item.index" :key="item.index">
-                        <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
+                        <i :class="item.icon"></i>{{ item.title }}
                     </el-menu-item>
                 </template>
             </template>
@@ -25,78 +25,80 @@
 
 <script>
     import bus from '../common/bus';
+
     export default {
         data() {
             return {
                 collapse: false,
                 items: [
                     {
-                        icon: 'ye-icon-wxbzhuye',
+                        icon: 'icon-shouye',
                         index: 'dashboard',
                         title: '系统首页'
                     },
                     {
-                        icon: 'el-icon-tickets',
-                        index: 'table',
-                        title: '基础表格'
-                    },
-                    {
-                        icon: 'el-icon-message',
-                        index: 'tabs',
-                        title: 'tab选项卡'
-                    },
-                    {
-                        icon: 'el-icon-date',
-                        index: '3',
-                        title: '表单相关',
+                        icon: 'el-icon-goods',
+                        index: '1',
+                        title: '商品管理',
                         subs: [
                             {
-                                index: 'form',
-                                title: '基本表单'
+                                icon: 'icon-shangpin',
+                                index: 'goodTypeManager',
+                                title: '商品类别管理'
                             },
                             {
-                                index: 'editor',
-                                title: '富文本编辑器'
-                            },
-                            {
-                                index: 'markdown',
-                                title: 'markdown编辑器'
-                            },
-                            {
-                                index: 'upload',
-                                title: '文件上传'
+                                icon: 'icon-shangpin',
+                                index: 'goodManager',
+                                title: '商品信息管理'
                             }
                         ]
                     },
                     {
-                        icon: 'el-icon-star-on',
-                        index: 'charts',
-                        title: 'schart图表'
+                        icon: 'icon-dingdan',
+                        index: 'orderManager',
+                        title: '订单管理'
                     },
                     {
-                        icon: 'el-icon-rank',
-                        index: 'drag',
-                        title: '拖拽列表'
+                        icon: 'icon-wuliuxiaocheche',
+                        index: 'logisticManager',
+                        title: '物流管理'
                     },
                     {
                         icon: 'el-icon-setting',
-                        index: 'management',
-                        title: '系统管理'
-                    },
-                    {
-                        icon: 'el-icon-error',
-                        index: '404',
-                        title: '404页面'
+                        index: '2',
+                        title: '系统管理',
+                        subs: [
+                            {
+                                icon: 'icon-drxx11',
+                                index: 'menuManager',
+                                title: '菜单管理',
+                            },
+                            {
+                                icon: 'icon-drxx10',
+                                index: 'userManager',
+                                title: '用户管理',
+                            },
+                            {
+                                icon: 'icon-drxx88',
+                                index: 'roleManager',
+                                title: '角色管理'
+                            },
+                            {
+                                icon:'icon-quanxianguanli',
+                                index: 'authorityManager',
+                                title: '权限管理'
+                            }
+                        ]
                     }
                 ]
             }
         },
-        computed:{
-            onRoutes(){
-                return this.$route.path.replace('/','');
+        computed: {
+            onRoutes() {
+                return this.$route.path.replace('/', '');
             }
         },
-        created(){
+        created() {
             // 通过 Event Bus 进行组件间通信，来折叠侧边栏
             bus.$on('collapse', msg => {
                 this.collapse = msg;
@@ -106,17 +108,19 @@
 </script>
 
 <style scoped>
-    .sidebar{
+    .sidebar {
         display: block;
         position: absolute;
         left: 0;
         top: 70px;
-        bottom:0;
+        bottom: 0;
     }
-    .sidebar-el-menu:not(.el-menu--collapse){
+
+    .sidebar-el-menu:not(.el-menu--collapse) {
         width: 250px;
     }
+
     .sidebar > ul {
-        height:100%;
+        height: 100%;
     }
 </style>
