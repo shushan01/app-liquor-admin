3c3e17cce7f5b40963c2cffaf51227f9ed71593c
yangyijun <1012293987@qq.com>
2018-05-21 21:47:46 +0800

    添加商品类别管理页面

--- app-view/src/components/page/good/addGood.vue
+++ app-view/src/components/page/good/addGood.vue
@@ -31,7 +31,7 @@
                         </el-select>
                     </el-form-item>
                     <el-form-item prop="price" label="价格">
-                        <el-input v-model="addForm.price" placeholder="请输入商品价格"></el-input>
+                        <el-input v-model.number="addForm.price" placeholder="请输入商品价格"></el-input>
                     </el-form-item>
                     <!-- <el-form-item label="推荐商品">
                          <el-select v-model="addForm.recommend" placeholder="请选是否是推荐商品">
@@ -39,8 +39,8 @@
                              <el-option :label="否" :value="0">否</el-option>
                          </el-select>
                      </el-form-item>-->
-                    <el-form-item prop="weigth" label="重量">
-                        <el-input v-model="addForm.weight" placeholder="请输入商品重量"></el-input>
+                    <el-form-item prop="weight" label="重量">
+                        <el-input v-model.number="addForm.weight" placeholder="请输入商品重量"></el-input>
                     </el-form-item>
                     <!-- <el-form-item label="活动状态">
                          <el-select v-model="addForm.activityStatus" placeholder="活动状态">
@@ -58,13 +58,13 @@
                          </el-select>
                      </el-form-item>-->
                     <el-form-item prop="emsFreight" label="EMS运费">
-                        <el-input v-model="addForm.emsFreight" placeholder="请输入商品EMS运费"></el-input>
+                        <el-input v-model.number="addForm.emsFreight" placeholder="请输入商品EMS运费"></el-input>
                     </el-form-item>
                     <el-form-item prop="expressFreight" label="快递运费">
-                        <el-input v-model="addForm.expressFreight" placeholder="请输入商品快递运费"></el-input>
+                        <el-input v-model.number="addForm.expressFreight" placeholder="请输入商品快递运费"></el-input>
                     </el-form-item>
                     <el-form-item prop="mailFreight" label="平邮运费">
-                        <el-input v-model="addForm.mailFreight" placeholder="请输入商品平邮运费"></el-input>
+                        <el-input v-model.number="addForm.mailFreight" placeholder="请输入商品平邮运费"></el-input>
                     </el-form-item>
                     <!-- <el-form-item prop="stockCnt" label="库存量">
                          <el-input v-model="addForm.stockCnt" placeholder="请输入商品库存量"></el-input>
--- app-view/static/js/good/addGood.js
+++ app-view/static/js/good/addGood.js
@@ -14,7 +14,6 @@ export default {
             uploadPictureDisplay: "display: none;",
             addAttrDisplay: "display: none;",
             finishDisplay: "display: none;",
-            // prevCss: "display: none;",
             nextCss: "",
             finishCss: "display: none;",
             dialogImageUrl: '',
@@ -32,6 +31,26 @@ export default {
                 name: [
                     {required: true, message: '请输入商品名称', trigger: 'blur'},
                     {max: 15, message: '商品名称不超过15个字符', trigger: 'blur'}
+                ],
+                price: [
+                    {required: true, message: '商品价格不能为空'},
+                    {type: 'number', message: '商品价格必须为数字值'}
+                ],
+                weight: [
+                    {required: true, message: '商品重量不能为空'},
+                    {type: 'number', message: '商品重量必须为数字值'}
+                ],
+                emsFreight: [
+                    {required: true, message: '商品EMS运费不能为空'},
+                    {type: 'number', message: '商品EMS运费必须为数字值'}
+                ],
+                expressFreight: [
+                    {required: true, message: '商品快递运费不能为空'},
+                    {type: 'number', message: '商品快递运费必须为数字值'}
+                ],
+                mailFreight: [
+                    {required: true, message: '商品平邮运费不能为空'},
+                    {type: 'number', message: '商品平邮运费必须为数字值'}
                 ]
             }
         }
@@ -54,84 +73,63 @@ export default {
     },
     computed: {},
     methods: {
-        // prev() {
-        //     this.active--;
-        //     if (this.active == 0) {
-        //         this.prevCss = "display: none;";
-        //     }
-        //     if (this.active == 1) {
-        //         this.prevCss = "display: block;";
-        //     }
-        //     if (this.active == 2) {
-        //         this.prevCss = "display: block;";
-        //     }
-        // },
         next() {
             if (this.active == 0) {
                 this.saveGood('addForm');
-                this.baseInfoDisplay = "display: none;";
-                this.uploadPictureDisplay = "display: block;";
-                // this.prevCss = "display: block;";
             }
             if (this.active == 1) {
                 this.uploadPictureDisplay = "display: none;";
                 this.addAttrDisplay = "display: block;"
                 this.nextCss = "display: none;";
                 this.finishCss = "display: block;";
+                this.active++;
             }
-            this.active++;
-        }
-        ,
+        },
         finish() {
             this.active++;
             this.uploadPictureDisplay = "display: none;";
             this.baseInfoDisplay = "display: none;";
             this.addAttrDisplay = "display: none;"
             this.finishDisplay = "display: block;"
-            // this.prevCss = "display: none;";
             this.nextCss = "display: none;";
             this.finishCss = "display: none;";
             this.active++;
-        }
-        ,
+        },
         goBack() {
             this.finishCss = "display: none;";
             this.$router.push("/goodManager");
-        }
-        ,
+        },
         handleExceed(files, fileList) {
             this.$message.error('上传图片个数超过限制!最多可以上传12张图片。');
         },
-        // handleSuccess(response, file, fileList) {
-        //     console.log(response, file, fileList)
-        //     this.fileList.push("1111.jpg")
-        //     // this.fileList = fileList;
-        // }
-        // ,
         //保存商品别信息
         saveGood(formName) {
-            // this.$refs[formName].validate((valid) => {
-            //     if (valid) {
-            this.$http.get(this.saveUrl, {
-                params: {
-                    name: this.addForm.name,
-                    categoryId: this.addForm.categoryId,
-                    price: this.addForm.price,
-                    weight: this.addForm.weight,
-                    emsFreight: this.addForm.emsFreight,
-                    expressFreight: this.addForm.expressFreight,
-                    mailFreight: this.addForm.mailFreight
-                }
-            }).then((res) => {
-                if (res.status == 200) {
-                    this.uploadData.goodCode = '';
-                    this.$message.success('添加商品基本信息成功');
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    this.$http.get(this.saveUrl, {
+                        params: {
+                            name: this.addForm.name,
+                            categoryId: this.addForm.categoryId,
+                            price: this.addForm.price,
+                            weight: this.addForm.weight,
+                            emsFreight: this.addForm.emsFreight,
+                            expressFreight: this.addForm.expressFreight,
+                            mailFreight: this.addForm.mailFreight
+                        }
+                    }).then((res) => {
+                        if (res.data.code == 0) {
+                            this.uploadData.goodCode = res.data.data;
+                            this.addBaseInfoSuccess = true;
+                            this.$message.success('添加商品基本信息成功');
+                            this.baseInfoDisplay = "display: none;";
+                            this.uploadPictureDisplay = "display: block;";
+                            this.active++;
+                        }
+                    });
+                } else {
+                    return false;
                 }
             });
-            //     } else {
-            //         return false;
-            //     }
-            // });
         },
         handleRemove(file, fileList) {
             console.log(file, fileList);
